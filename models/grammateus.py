5fbf61b326f71a9ec0fa342707073fff138f2cd2
Ian W. Scott <iscott@tyndale.ca>
2012-01-26 23:06:43 -0500

    Minor bugfixes.

--- models/grammateus.py
+++ models/grammateus.py
@@ -1,8 +1,8 @@
 # coding: utf8
 import datetime
 
-db.define_table('docs', 
-    Field('name'), 
+db.define_table('docs',
+    Field('name'),
     Field('filename'),
     Field('editor', db.auth_user, default = auth.user_id),
     Field('assistant_editor', db.auth_user, default = auth.user_id),
@@ -13,16 +13,16 @@ db.define_table('docs',
     Field('status', 'text'),
     Field('bibliography', 'text'),
     Field('sigla', 'text'),
-    Field('copyright', 'text'),    
+    Field('copyright', 'text'),
     format='%(name)s')
 
 db.define_table('biblio',
     Field('record'),
     format='%(record)s')
-    
-db.define_table('bugs', 
-    Field('title'), 
+
+db.define_table('bugs',
+    Field('title'),
     Field('body', 'text'),
     Field('poster', db.auth_user, default = auth.user_id),
-    Field('post_date', 'datetime', default = datetime.date.now()),
+    Field('post_date', 'datetime'),
     format='%(title)s')
