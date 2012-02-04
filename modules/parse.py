38c5d2afbb2d06a11d29b1001156e75c2b23992f
jeffder <jderuyter@iinet.net.au>
2012-02-04 18:39:06 +1000

    Update parser to extract unit data from xml

--- modules/parse.py
+++ modules/parse.py
@@ -46,14 +46,13 @@ class BookParser(object):
             version_dict['organisation_levels'] = len(version.xpath('divisions/division'))
 
             version_dict['text_structure'] = self.text_structure(version.xpath('text')[0])
-#            print version_dict['text_structure']
 
             info['versions'].append(version_dict)
 
         return info
 
 
-    def text_structure(self, text):
+    def text_structure(self, text, delimiter=':'):
         '''
         Extract the div structure from a given text tag.
         '''
@@ -64,21 +63,37 @@ class BookParser(object):
 
             children = OrderedDict()
             for child_div in div.xpath('div'):
-                child_key = child_div.xpath('@number')[0]
-                child_value = self.text_structure(child_div)
+                child_structure = self.text_structure(child_div)
 
-                children[child_key] = child_value
+                if child_structure:
+                    for k, v in child_structure:
+                        key = '%s%s%s' % (parent_key, delimiter, k)
+                        parent[key] = v
 
-            if children.keys():
-                parent[parent_key] = children
-            else:
-                parent[parent_key] = None
+                        # Remove child keys
+                        del child_structure[k]
 
-        if parent:
-            return parent
+                else:
+                    # Child div has no children so extract the unit data
+                    readings = {}
+                    for unit in child_div.xpath('unit'):
+                        unit_dict = {}
 
-        return None
+                        unit_number = unit.xpath('@id')[0]
 
+                        reading_dict = {}
+                        for reading in unit.xpath('reading'):
+                            mss = reading.xpath('@mss')[0]
+                            for m in mss.strip().split():
+                                reading_dict[m] = reading.text
+
+                        readings[unit_number] = reading_dict
+
+                    child_key = child_div.xpath('@number')[0]
+                    key = '%s%s%s' % (parent_key, delimiter, child_key)
+                    parent[key] = readings
+
+        return parent
 
 
     def render(self):
