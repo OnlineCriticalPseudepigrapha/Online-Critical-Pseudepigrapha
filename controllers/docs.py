5ba387d0f0aea16bc9e70d14405de55bf3b345f9
Ian W. Scott <scottianw@gmail.com>
2012-03-06 14:45:13 -0500

    Added two new controller methods in controllers/docs.py (with corresponding views) to begin building reading interface with new parsing class: text() and section(). Text() initiates the reader page and then loads section() in a div via ajax. Section() can then be refreshed via ajax without calling text() again.

--- controllers/docs.py
+++ controllers/docs.py
@@ -3,13 +3,55 @@ from lxml import etree
 
 from parse import BookParser
 
+"""
+List of session objects used in text display:
+session.filename
+session.info -- dict; parsed results of BookParser class for current doc
+session.title -- string; title of current doc
+session.versions -- list; names of all versions in current doc
+"""
+
 def index():
     filename = request.args[0]
     return dict(filename = filename)
 
 def text():
     filename = request.args[0]
-    return dict(filename = filename)
+    #TODO: provide fallback and prompt if no filename is given in url
+    
+    #url to pass to web2py load helper in view to load doc section via ajax
+    load_url = URL('docs', 'section.load', args=filename)
+
+    return dict(load_url = load_url, filename = filename)
+
+def section():
+    """
+    populates a single text pane via the section.load view (refreshable via ajax)
+    """
+    #get filename from end of url and parse the file with BookParser class
+    filename = request.args[0]
+    print filename
+    book_file = 'applications/grammateus3/static/docs/%s.xml' % filename
+    p = BookParser(book_file)
+    info = p.book_info()
+    #get title of document
+    title = info['title']
+    #get names of all versions of current doc and select the version to display
+    #use second url argument as version name if present, otherwise default to first version
+    versions = [version['title'] for version in info['versions']]     
+    if len(request.args) < 2:
+        current_version = versions[0]
+    else:
+        current_version = request.args[1].replace('_', ' ')
+        #move selected version to top of list for selectbox
+        i = versions.index(current_version)
+        versions.insert(0, versions.pop(i))
+
+    ref = dict()
+        
+    print versions, current_version
+    return dict(title = title, versions = versions, current_version = current_version, 
+                info = info, filename = filename)
 
 def test():
     filename = request.args[0]
