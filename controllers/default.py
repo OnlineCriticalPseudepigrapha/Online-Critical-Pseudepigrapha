311ff18e1868c131c1372f3cb31af0161dd3fd0c
Ian W. Scott <iscott@tyndale.ca>
2012-05-29 13:32:09 -0400

    Lots of changes, including slight re-structuring of output in parse.py module.

--- controllers/default.py
+++ controllers/default.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 # this file is released under public domain and you can use without limitations
 
-#########################################################################
-## This is a samples controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
-
 def index():
     """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+    Present main index page.
+
+    Send dictionary of document records to the view views/default/index.html
     """
     docrows = db(db.docs.id > 0).select()
     return dict(docrows = docrows)
