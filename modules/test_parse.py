aa6e42da5305bdd8d7681bdf82b05b7c7cffdf00
Tamas Mako <tom@greenplug.hu>
2013-03-15 19:04:48 +0100

    add div xpath portion generator in arbitrary depth and tests cases for it

--- modules/test_parse.py
+++ modules/test_parse.py
@@ -30,3 +30,90 @@ def test_validation(sample_book):
         book.validate(open(DTD_FILE))
     # validation with success
     assert sample_book.validate(open(DTD_FILE)) is None
+
+
+def test_gen_divpath_open_end():
+    assert Book.gen_divpath(start_div=(1,)) == ["div[@number>=1]"]
+    assert Book.gen_divpath(start_div=(1, 2)) == ["div[@number=1]/div[@number>=2]",
+                                                  "div[@number>1]/div"]
+    assert Book.gen_divpath(start_div=(1, 2, 3)) == ["div[@number=1]/div[@number=2]/div[@number>=3]",
+                                                     "div[@number=1]/div[@number>2]/div",
+                                                     "div[@number>1]/div/div"]
+
+
+def test_gen_divpath_open_start():
+    assert Book.gen_divpath(end_div=("7",)) == ["div[@number<=7]"]
+    assert Book.gen_divpath(end_div=("7", "8")) == ["div[@number<7]/div",
+                                                    "div[@number=7]/div[@number<=8]"]
+    assert Book.gen_divpath(end_div=("7", "8", "9")) == ["div[@number<7]/div/div",
+                                                         "div[@number=7]/div[@number<8]/div",
+                                                         "div[@number=7]/div[@number=8]/div[@number<=9]"]
+
+
+def test_gen_divpath_same_depth_1():
+    assert Book.gen_divpath((1,), (1,)) == ["div[@number=1]"]
+    assert Book.gen_divpath((1,), (2,)) == ["div[@number>=1 and @number<=2]"]
+    assert Book.gen_divpath((1,), (3,)) == ["div[@number>=1 and @number<=3]"]
+
+
+def test_gen_divpath_same_depth_2():
+    assert Book.gen_divpath((1, 2), (1, 2)) == ["div[@number=1]/div[@number=2]"]
+    assert Book.gen_divpath((1, 2), (1, 3)) == ["div[@number=1]/div[@number>=2 and @number<=3]"]
+    assert Book.gen_divpath((1, 2), (1, 4)) == ["div[@number=1]/div[@number>=2 and @number<=4]"]
+    # TODO: the second path is unnecessary, we should optimize it
+    assert Book.gen_divpath((1, 2), (2, 4)) == ["div[@number=1]/div[@number>=2]",
+                                                "div[@number>1 and @number<2]/div",
+                                                "div[@number=2]/div[@number<=4]"]
+    assert Book.gen_divpath((1, 2), (3, 4)) == ["div[@number=1]/div[@number>=2]",
+                                                "div[@number>1 and @number<3]/div",
+                                                "div[@number=3]/div[@number<=4]"]
+
+
+def test_gen_divpath_same_depth_3():
+    assert Book.gen_divpath((1, 2, 3), (1, 2, 3)) == ["div[@number=1]/div[@number=2]/div[@number=3]"]
+    assert Book.gen_divpath((1, 2, 3), (1, 2, 4)) == ["div[@number=1]/div[@number=2]/div[@number>=3 and @number<=4]"]
+    # TODO: the second path is unnecessary, we should optimize it
+    assert Book.gen_divpath((1, 2, 3), (1, 3, 4)) == ["div[@number=1]/div[@number=2]/div[@number>=3]",
+                                                      "div[@number=1]/div[@number>2 and @number<3]/div",
+                                                      "div[@number=1]/div[@number=3]/div[@number<=4]"]
+    assert Book.gen_divpath((1, 2, 3), (2, 3, 4)) == ["div[@number=1]/div[@number=2]/div[@number>=3]",
+                                                      "div[@number=1]/div[@number>2]/div",
+                                                      "div[@number>1 and @number<2]/div/div",
+                                                      "div[@number=2]/div[@number<3]/div",
+                                                      "div[@number=2]/div[@number=3]/div[@number<=4]"]
+
+
+def test_gen_divpath_invalid_args():
+    assert Book.gen_divpath((1, 2), (1,)) == []
+    assert Book.gen_divpath((1, 2, 3), (1, 2)) == []
+    assert Book.gen_divpath((2, 3), (1,)) == []
+    assert Book.gen_divpath((2,), (1,)) == []
+    assert Book.gen_divpath((2,), (1, 2)) == []
+    assert Book.gen_divpath((2,), (1, 2, 3)) == []
+
+
+def test_gen_divpath_diff_depth_1():
+    assert Book.gen_divpath((1, 2), (3,)) == ["div[@number=1]/div[@number>=2]",
+                                              "div[@number>1 and @number<=3]/div"]
+    assert Book.gen_divpath((1, 2, 3), (4,)) == ["div[@number=1]/div[@number=2]/div[@number>=3]",
+                                                 "div[@number=1]/div[@number>2]/div",
+                                                 "div[@number>1 and @number<=4]/div/div"]
+    assert Book.gen_divpath((1, 2, 3), (4, 5)) == ["div[@number=1]/div[@number=2]/div[@number>=3]",
+                                                   "div[@number=1]/div[@number>2]/div",
+                                                   "div[@number>1 and @number<4]/div/div",
+                                                   "div[@number=4]/div[@number<=5]"]
+
+
+def test_gen_divpath_diff_depth_2():
+    assert Book.gen_divpath((1,), (1, 2)) == ["div[@number=1]/div[@number<=2]"]
+    assert Book.gen_divpath((1,), (2, 3)) == ["div[@number>=1 and @number<2]",
+                                              "div[@number=2]/div[@number<=3]"]
+    assert Book.gen_divpath((1,), (3, 4)) == ["div[@number>=1 and @number<3]",
+                                              "div[@number=3]/div[@number<=4]"]
+    assert Book.gen_divpath((1,), (3, 4, 5)) == ["div[@number>=1 and @number<3]",
+                                                 "div[@number=3]/div[@number<4]/div",
+                                                 "div[@number=3]/div[@number=4]/div[@number<=5]"]
+    assert Book.gen_divpath((1, 2), (3, 4, 5)) == ["div[@number=1]/div[@number>=2]",
+                                                   "div[@number>1 and @number<3]/div",
+                                                   "div[@number=3]/div[@number<4]/div",
+                                                   "div[@number=3]/div[@number=4]/div[@number<=5]"]
