ccfb12f6a81e6659d37370768adbde0b86329ef7
Ian W. Scott <scottianw@gmail.com>
2019-02-05 20:00:15 -0500

    Adding test file for new dts_server module.

---
+++ test/modules/test_dts_server.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+run tests from web2py root directory with:
+
+    python3 -m pytest -xvvs applications/grammateus3/test/modules/test_parse.py
+"""
